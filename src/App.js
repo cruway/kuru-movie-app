0f85a38bab354ec833d0556b09b3896548fabf42
sim wook <astralkaiser89@gmail.com>
2022-09-14 20:26:33 +0900

    useEffect test

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ function App() {
     const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("i run all the time");
+    const onChange = (event) => setKeyword(event.target.value);
     useEffect(() => {
-        console.log("CALL THE API...");
+        console.log("I run ony once.");
     }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("I run when 'keyword&counter' changes.");
+    }, [keyword, counter]);
     return (
     <div>
-        <input type="text" placeholder="Search here..."/>
-      <h1>{counter}</h1>
+        <input
+            value={keyword}
+            onChange={onChange} t
+            ype="text"
+            placeholder="Search here..."/>
+        <h1>{counter}</h1>
         <button onClick={onClick}>click me</button>
     </div>
   );
